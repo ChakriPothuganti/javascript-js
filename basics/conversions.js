fb9057f52108693c277914edf51f54318b3d84f9
ChakriPothuganti <chakripothuganti2120@gmail.com>
2024-01-10 15:36:47 +0530

    inclusion of operations in datatypes conversions

--- basics/conversions.js
+++ basics/conversions.js
@@ -1,11 +1,11 @@
 let score = "100"
 
-console.log(typeof score)
-console.log(typeof(score))
+//console.log(typeof score)
+//console.log(typeof(score))
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber)
 
 /*
     if score = 100  (number) then valueInNumber = 100
@@ -19,7 +19,7 @@ console.log(valueInNumber)
 let isLoggedIn = "java"
 
 let booleanValue = Boolean(isLoggedIn)
-console.log(booleanValue)
+//console.log(booleanValue)
 
 /*
 if isLoogedIn is a non-empty string then true else false
@@ -27,5 +27,32 @@ if isLoogedIn is a non-empty string then true else false
 
 let num = 21
 let stringNumber = String(num)
-console.log(num);
-console.log(typeof num)
+//console.log(num);
+//console.log(typeof num)
+
+// -----------------Operations--------------//
+
+let str1 = "hello"
+let str2 = " world"
+let str3 = str1 + str2
+// console.log(str3) 
+
+// console.log("1" + 2)
+// console.log(1 + "2")
+// console.log("1" + 2 + 3)
+// console.log(1 + 2 + "3") // evaluation is done up to 2 then concatenated with 3
+// console.log(1 + "2" + 3)
+
+//console.log(+true) // 1
+//console.log(+"") // 0
+
+let num1,num2,num3
+
+num1 = num2 =num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter)
+
+// for more info
+// https://tc39.es/ecma262/#sec-abstract-operations
