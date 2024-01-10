f720b739fdb7da89cb56c48c3aa0f5731c3b9e78
ChakriPothuganti <chakripothuganti2120@gmail.com>
2024-01-10 14:52:38 +0530

    about datatypes

---
+++ basics/datatypes.js
@@ -0,0 +1,20 @@
+console.log("Chakri")
+
+ let name = "Chakri"
+ let age = 20
+ let isLoggedIn = false
+ let value = null
+
+ // number => 2 to the power 53
+ // bigint
+ // String
+ // boolean => true or false
+ // null => standalone value
+ // undefined
+ // symbol => uniqueness
+
+
+ // object
+
+ console.log(typeof null)
+ console.log(typeof undefined)
