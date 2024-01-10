c9e5314907a5a6643efa9f3376bb07e3bdf5f204
ChakriPothuganti <chakripothuganti2120@gmail.com>
2024-01-10 14:24:11 +0530

    basics is added

---
+++ basics/variables.js
@@ -0,0 +1,15 @@
+const accountId = 12345678
+/*
+the value of accountId can't be changed further as it is intialised as const
+*/
+let accountEmail = "Chakri@gmail.com"
+var accountPassword = 12345
+accountCity = "Hyderabad"
+
+/*
+var is not used much because of the scope problem.
+*/
+
+accountEmail = "learning@gmail.com"
+accountPassword = 54321
+accountCity = "Pocharam"
