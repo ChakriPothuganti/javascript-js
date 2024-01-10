dacf1605ba3eac71cdc1a1db10b8fc3b1a152cfd
ChakriPothuganti <chakripothuganti2120@gmail.com>
2024-01-10 14:28:24 +0530

    variables

--- basics/variables.js
+++ basics/variables.js
@@ -5,6 +5,7 @@ the value of accountId can't be changed further as it is intialised as const
 let accountEmail = "Chakri@gmail.com"
 var accountPassword = 12345
 accountCity = "Hyderabad"
+let accountState
 
 /*
 var is not used much because of the scope problem.
@@ -13,3 +14,6 @@ var is not used much because of the scope problem.
 accountEmail = "learning@gmail.com"
 accountPassword = 54321
 accountCity = "Pocharam"
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+/* console.table is used to display the output in the form of a table.*/
