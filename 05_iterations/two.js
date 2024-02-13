20ce2ebb1e7f5f7dec763c054c3b36c27bd3ce97
ChakriPothuganti <chakriofficial1274@gmail.com>
2024-02-13 22:15:45 +0530

    all about while and do while

---
+++ 05_iterations/two.js
@@ -0,0 +1,21 @@
+
+// let index = 0
+// while (index <= 10) {
+//     console.log(`Value of index is ${index}`);
+//     index += 2
+// }
+
+// let myArr = ['naruto','sasuke','itachi']
+
+// let arr = 0
+// while(arr < myArr.length){
+//     console.log(`Value is ${myArr[arr]}`);
+//     arr += 1
+// }
+
+let score = 1 
+
+do {
+    console.log(`Score is ${score}`);
+    score++
+} while(score <= 10)
